86c1a0bc99128ffcdc5a2ff941e41030e4c92f10
eric nuthu <ericnuthu@MacBookAir.stl-horizon.com>
2025-02-03 14:53:56 +0300

    authcontroller to handle registration and login with JWT

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['token' => JWTAuth::fromUser($user)]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!$token = JWTAuth::attempt($request->only('email', 'password'))) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return response()->json(compact('token'));
+    }
+}
+
