538ce8a3f0abb38b205d93d65f1c71e782fb147f
eric nuthu <ericnuthu@MacBookAir.stl-horizon.com>
2025-02-19 09:10:42 +0300

    created mail service

---
+++ app/Mail/TestMail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TestMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct()
+    {
+        //
+    }
+
+    public function build()
+    {
+        return $this->from('nuthuerick147@gmail.com')
+                    ->subject('Test Email')
+                    ->view('emails.test');
+    }
+}
+
